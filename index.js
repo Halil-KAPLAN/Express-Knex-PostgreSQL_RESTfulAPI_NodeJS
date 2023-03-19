ca52b6e77218fd35e49e214392001e8a90705f21
Halil KAPLAN <halil-kaplan@windowslive.com>
2023-03-19 06:04:14 +0300

    Added router to requests

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
 const express = require("express");
-const data = require("./data");
+const actorsRouter = require("./routers/actorsRouter");
 const server = express();
-
-const host = "127.0.0.1";
-const port = 3000;
+server.use(express.json());
 
 server.get("/", (req, res) => {
   res.send("Hi Express");
 });
 
-server.get("/actors", (req, res) => {
-  res.status(200).json(data);
-});
+server.use("/actors", actorsRouter);
 
-server.get("/actors/:id", (req, res) => {
-  const { id } = req.params;
-  const actor = data.find((actor) => actor.id === parseInt(id));
-  if (actor) {
-    res.status(200).json(actor);
-  } else {
-    res.status(404).send("Actor is not found!");
-  }
-});
-
+const host = "127.0.0.1";
+const port = 5000;
 server.listen(port, host, () => {
   console.log(`Listening ${host}:${port} address`);
 });
---
+++ routers/actorsRouter.js
@@ -0,0 +1,58 @@
+const router = require("express").Router();
+
+let data = require("../data");
+
+
+router.get("/", (req, res) => {
+  res.status(200).json(data);
+});
+
+let testNextId = 4;
+router.post("/", (req, res) => {
+  let new_actor = req.body;
+  new_actor.id = testNextId;
+  testNextId++;
+  data.push(new_actor);
+  res.status(201).json(new_actor);
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const updateValues = req.body;
+
+  const updatedActor = data.find((actor) => actor.id === Number(id));
+  if (updatedActor) {
+    let reportUpdated;
+    data = data.map((actor) =>
+      actor.id === Number(id)
+        ? (reportUpdated = { ...actor, ...updateValues })
+        : actor
+    );
+    res.status(200).json(reportUpdated);
+  } else {
+    res.status(404).json({ errorMessage: "Actor is not found!" });
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const deletedActor = data.find((actor) => actor.id === Number(id));
+  if (deletedActor) {
+    data = data.filter((actor) => actor.id !== Number(deletedActor.id));
+    res.status(204).end();
+  } else {
+    res.status(404).json({ errorMessage: "Actor is not found!" });
+  }
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const actor = data.find((actor) => actor.id === parseInt(id));
+  if (actor) {
+    res.status(200).json(actor);
+  } else {
+    res.status(404).send("Actor is not found!");
+  }
+});
+
+module.exports = router;
